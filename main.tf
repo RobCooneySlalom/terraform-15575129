ba0dedc3a8b27d37494e6c10cae1731fad176897
RobCooneySlalom <144217857+RobCooneySlalom@users.noreply.github.com>
2023-10-13 10:00:42 +0100

    TRFRM-04: fix naming issue

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" {
+module "vpc" "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
     version = "5.1.2"
 
@@ -48,7 +48,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_rules"
 
-  vpc_id = module.vpc.public_subnets[0]
+  vpc_id = module.vpc.blog_vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
