e656c27147b5ae1d33e2dc6a9cd32d2eaf8fbe43
RobCooneySlalom <144217857+RobCooneySlalom@users.noreply.github.com>
2023-10-13 10:03:28 +0100

    TRFRM-04: fix vpc issue

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-module "vpc" "blog_vpc" {
+module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
     version = "5.1.2"
 
@@ -22,6 +22,7 @@ module "vpc" "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -48,7 +49,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_rules"
 
-  vpc_id = module.vpc.blog_vpc.public_subnets[0]
+  vpc_id = module.blog_vpc.public_subnets[0]
 
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
