4e7eac709505f21b6c10415039aa1f4bc1a2a749
RobCooneySlalom <144217857+RobCooneySlalom@users.noreply.github.com>
2023-10-13 15:27:09 +0100

    TRFRM-04: refactor using variables

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = [var.ami_filter.name]
   }
 
   filter {
@@ -11,25 +11,25 @@ data "aws_ami" "app_ami" {
     values = ["hvm"]
   }
 
-  owners = ["979382823631"] # Bitnami
+  owners = [var.ami_filter.owner]
 }
 
 module "blog_vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-    version = "5.1.2"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
 
-  name = "terraform-course-dev"
-  cidr = "10.0.0.0/16"
+  name = var.env.name
+  cidr = "${var.env.network_prefix}.0.0/16"
 
   azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["${var.env.network_prefix}.1.0/24", "${var.env.network_prefix}.2.0/24", "${var.env.network_prefix}.3.0/24"]
+  public_subnets  = ["${var.env.network_prefix}.101.0/24", "${var.env.network_prefix}.102.0/24", "${var.env.network_prefix}.103.0/24"]
 
   enable_nat_gateway = false
 
   tags = {
-    Terraform = "true"
-    Environment = "dev"
+    Terraform   = "true"
+    Environment = var.env.name
   }
 }
 
@@ -37,13 +37,13 @@ module "blog_autoscaling" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "7.1.0"
   
-  name     = "blog-asg"
-  min_size = 1
-  max_size = 2
+  name     = "${var.env.name}-blog-asg"
+  min_size = var.autoscaling_config.min_size
+  max_size = var.autoscaling_config.max_size
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns
-  security_groups = [module.blog_sg.security_group_id]
+  security_groups     = [module.blog_sg.security_group_id]
 
   image_id      = data.aws_ami.app_ami.id
   instance_type = var.instance_type
@@ -53,7 +53,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 8.0"
 
-  name = "blog-alb"
+  name = "${var.env.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -63,7 +63,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.env.name}-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -79,20 +79,21 @@ module "blog_alb" {
   ]
 
   tags = {
-    Environment = "dev"
+    Environment = var.env.name
   }
 }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name = "blog_rules"
+
+  name = "${var.env.name}-blog-rules"
 
   vpc_id = module.blog_vpc.vpc_id
 
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
 
-  egress_rules = ["all-all"]
+  egress_rules       = ["all-all"]
   egress_cidr_blocks = ["0.0.0.0/0"]
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,45 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+
+variable "ami_filter" {
+  description = "Filter values when searching for AMI"
+
+  type = object({
+    name  = string
+    owner = string
+  })
+
+  default = {
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
+  }
+}
+
+variable "env" {
+  description = "Development environment config"
+
+  type = object({
+    name           = string
+    network_prefix = string
+  })
+
+  default = {
+    name           = "terraform-course-dev"
+    network_prefix = "10.0"
+  }
+}
+
+variable "autoscaling_config" {
+  description = "Configuration for EC2 autoscaling group"
+
+  type = object({
+    min_size = number
+    max_size = number
+  })
+
+  default = {
+    min_size = 1
+    max_size = 2
+  }
+}
